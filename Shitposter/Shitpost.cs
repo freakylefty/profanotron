4b329e8b8624fa9187788ed5fb6fc8acc66120d6
Andy Burn <andy@aburn.org.uk>
2016-11-18 16:18:35 +0000

    Allow editing of text before posting; disable controls while posting

--- Shitposter/Shitpost.cs
+++ Shitposter/Shitpost.cs
@@ -16,20 +16,28 @@ namespace Shitposter {
 
         private void Generate(object sender, EventArgs e) {
             string result = generator.Profane();
-            profanity.Text = result;
+            textProfanity.Text = result;
         }
 
         private void button2_Click(object sender, EventArgs e) {
             if (MessageBox.Show("Are you sure you wish to post this to Twitter?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
-                twitterPost(profanity.Text);
+                twitterPost(textProfanity.Text);
         }
 
         private void twitterPost(string value) {
+            btnNext.Enabled = btnPost.Enabled = textProfanity.Enabled = false;
             if (!LoginPanel.Login()) {
                 MessageBox.Show("Could not connect to Twitter account", "Shitpost", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                btnNext.Enabled = btnPost.Enabled = textProfanity.Enabled = true;
                 return;
             }
             ITweet result = Tweet.PublishTweet(value);
+            if (result.IsTweetPublished) {
+                MessageBox.Show("Tweet published", "Shitpost", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            } else {
+                MessageBox.Show("Tweet could not be published", "Shitpost", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            btnNext.Enabled = btnPost.Enabled = textProfanity.Enabled = true;
         }
     }
 }
